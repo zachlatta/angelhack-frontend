aec5c916b6efd6c095c415fac6df10913e3bd3a1
Zach Latta <zach@zachlatta.com>
2014-05-18 11:37:44 -0700

    Initial commit.

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('angelhackFrontendApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
